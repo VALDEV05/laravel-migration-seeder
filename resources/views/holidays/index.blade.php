3032ac95b2e0fef54a0467f7219ba951a9fc7b96
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-01-18 18:02:16 +0100

    Completed layout and add style now I proceed with the help of faker

--- resources/views/holidays/index.blade.php
+++ resources/views/holidays/index.blade.php
@@ -2,5 +2,49 @@
 
 
 @section('content')
-    <h1 class="text-center">Holidays</h1>
+    <div class="container">
+        <h1 id="main_title" class="py-4">Holidays</h1>
+        <section class="holiday">
+            <div class="row">
+                @foreach ($holidays as $holiday)
+                    <div class="col-4">
+                        <div class="card holiday m-4 shadow">
+                            <div class="card-img holiday">
+                                <img id="card-image" src="{{$holiday->image}}">
+                            </div>
+                            <div class="card-price holiday">
+                                <button class="btn btn-light price shadow-sm">
+                                    <p class="mb-0">da 1200 &euro; .pp</p>
+                                    
+                                </button>
+                            </div>
+                            <div class="card-body holiday">
+                                <div class="card-tag text-uppercase ms-2 mt-1">
+                                    <p class="mb-0 text-sm">
+                                        <i class="fas fa-suitcase"></i> Vacanze
+                                    </p>
+                                </div>
+                               <div class="card-text">
+                                   <div id="title" class="text-center">
+                                       <p class="mb-0 text-lg"> 
+                                           Mamma mia tutti a {{$holiday->city_destination}}
+                                       </p>
+                                   </div>
+                                   <div class="card-info-trip ms-2 mt-3">
+                                       <p class="mb-0 text-sm">
+                                           {{$holiday->date_departure}}<i class="fa fa-arrow-right"></i>{{$holiday->date_destination}}
+                                       </p>
+                                   </div>
+                                </div>
+                            </div>
+                            <a href="{{route('home')}}"><i id="card-go" class="fa fa-arrow-right"></i></a>
+                            
+                        </div>
+                    </div>
+                @endforeach 
+            </div>
+            
+        </section>
+    </div>
+    
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
 
-     <p class="text-center fixed-bottom">
+     <p class="fixed-bottom">
          <i class="fas fa-copyright fa-sm fa-fw"></i> VALDEV05 copyright 2022
      </p>
 
